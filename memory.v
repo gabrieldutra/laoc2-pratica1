2104f5faa4f7081be61fbb24e76305c55d61db5e
Gabriel Dutra <nesk.frz@gmail.com>
2018-04-06 16:34:49 -0300

    Leitura e escrita memória

--- memory.v
+++ memory.v
@@ -15,10 +15,11 @@ module memory (
 	
 	
 	// memory
-	reg [4:0] memAddress;
-	reg [7:0] memData;
-	reg memWren;
+	reg [4:0] memAddress = 5'b00000;
+	reg [7:0] memData = 8'b00000000;
+	reg memWren = 0;
 	wire [7:0] memOutput;
+	reg [1:0] memCount = 2'b00;
 	
 	// cache
 	reg [16:0] cache[3:0];
@@ -28,12 +29,13 @@ module memory (
 	reg outputValid = 0;
 	
 	// current
-	reg [4:0] currentAddress = 8'b00000000;
+	reg [4:0] currentAddress = 5'b00000;
 	reg [7:0] currentData;
 	reg currentWren = 0;
 	
 	// local
 	reg [2:0] count = 3'b000;
+	reg [1:0] writeAddress = 2'b00;
 	
 	initial
 		begin
@@ -51,7 +53,11 @@ module memory (
 				outputValid = 0;
 				currentAddress = address;
 				currentWren = wren;
+				currentData = data;
+				memCount = 0;
 			end
+		
+		// Leitura e Escrita Cache - Cache Hit
 		
 		if (outputValid == 0 && currentAddress == cache[0][12:8] && cache[0][16:16]) // Cache Hit - 1a linha
 			begin
@@ -131,7 +137,69 @@ module memory (
 					cache[0][14:13] = cache[0][14:13] + 2'b01;
 				cache[3][14:13] = 2'b00;
 			end
-		
+			
+		// Cache Miss
+		if (outputValid == 0)
+			begin
+				// Pega o LRU = 3
+				if (cache[0][14:13] == 2'b11) writeAddress = 2'b00;
+				if (cache[1][14:13] == 2'b11) writeAddress = 2'b01;
+				if (cache[2][14:13] == 2'b11) writeAddress = 2'b10;
+				if (cache[3][14:13] == 2'b11) writeAddress = 2'b11;			
+					
+				if (memWren == 1) // Quando a escrita na memória do Write Back é feita
+					begin
+						memWren = 0;
+						cache[writeAddress][15:15] = 0;
+					end
+					
+				if (cache[writeAddress][15:15] == 1) // Write Back
+					begin
+						memAddress = cache[writeAddress][12:8];
+						memData = cache[writeAddress][7:0];
+						memWren = 1;
+					end
+					else begin // Só altera o registro quando o dado não está mais dirty
+						if (currentWren) // Escrita no caso de miss
+							begin
+								cache[writeAddress][16:16] = 1'b1; // Valid
+								cache[writeAddress][15:15] = 1'b1; // Dirty
+								cache[writeAddress][12:8] = currentAddress;
+								cache[writeAddress][7:0] = currentData;
+								outputValid = 1;
+								// Update LRU
+								if (cache[writeAddress][14:13] > cache[1][14:13]) 
+									cache[1][14:13] = cache[1][14:13] + 2'b01;
+								if (cache[writeAddress][14:13] > cache[2][14:13]) 
+									cache[2][14:13] = cache[2][14:13] + 2'b01;
+								if (cache[writeAddress][14:13] > cache[0][14:13]) 
+									cache[0][14:13] = cache[0][14:13] + 2'b01;
+								cache[writeAddress][14:13] = 2'b00;
+							end
+							else begin // Leitura no caso de miss
+								if (memCount == 2'b11) // Clock - Leitura 
+									begin
+										cache[writeAddress][16:16] = 1'b1; // Valid
+										cache[writeAddress][15:15] = 1'b0; // Dirty
+										cache[writeAddress][12:8] = currentAddress;
+										cache[writeAddress][7:0] = memOutput;
+										// Update LRU
+										if (cache[writeAddress][14:13] > cache[1][14:13]) 
+											cache[1][14:13] = cache[1][14:13] + 2'b01;
+										if (cache[writeAddress][14:13] > cache[2][14:13]) 
+											cache[2][14:13] = cache[2][14:13] + 2'b01;
+										if (cache[writeAddress][14:13] > cache[0][14:13]) 
+											cache[0][14:13] = cache[0][14:13] + 2'b01;
+										cache[writeAddress][14:13] = 2'b00;
+									end
+								memAddress = currentAddress;
+								memData = 8'b00000000;
+								memWren = 0;
+								memCount = memCount + 2'b01;
+							end
+					end
+			end
+			
 		
 	end
 	
--- simulation/modelsim/pratica1_run_msim_rtl_verilog.do.bak11
+++ simulation/modelsim/pratica1_run_msim_rtl_verilog.do.bak11
@@ -5,8 +5,8 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -vlog01compat -work work +incdir+E:/laoc2-pratica1 {E:/laoc2-pratica1/ramlpm.v}
-vlog -vlog01compat -work work +incdir+E:/laoc2-pratica1 {E:/laoc2-pratica1/pratica1.v}
-vlog -vlog01compat -work work +incdir+E:/laoc2-pratica1 {E:/laoc2-pratica1/bcd_7seg.v}
-vlog -vlog01compat -work work +incdir+E:/laoc2-pratica1 {E:/laoc2-pratica1/memory.v}
+vlog -vlog01compat -work work +incdir+C:/Users/Aluno/Documents/laoc2-pratica1 {C:/Users/Aluno/Documents/laoc2-pratica1/ramlpm.v}
+vlog -vlog01compat -work work +incdir+C:/Users/Aluno/Documents/laoc2-pratica1 {C:/Users/Aluno/Documents/laoc2-pratica1/pratica1.v}
+vlog -vlog01compat -work work +incdir+C:/Users/Aluno/Documents/laoc2-pratica1 {C:/Users/Aluno/Documents/laoc2-pratica1/bcd_7seg.v}
+vlog -vlog01compat -work work +incdir+C:/Users/Aluno/Documents/laoc2-pratica1 {C:/Users/Aluno/Documents/laoc2-pratica1/memory.v}
 
